0837d4657a46a2fde4ca0ef6a054e11a2a40b8b7
tanhongbin-php <55487039+tanhongbin-php@users.noreply.github.com>
2024-09-10 17:11:45 +0800

    Add files via upload

--- src/config/plugin/thb/rabbitmq/rabbitmq.php
+++ src/config/plugin/thb/rabbitmq/rabbitmq.php
@@ -2,10 +2,15 @@
 return [
     //默认redis队列配置
     'default' => [
-        'host' => envs('RABBITMQ_HOST','127.0.0.1'),
-        'port' => envs('RABBITMQ_PORT','5672'),
-        'user' => envs('RABBITMQ_USER','thb'),
-        'password' => envs('RABBITMQ_PASSWORD','thb910626'),
-        'vhost' => envs('RABBITMQ_VHOST','/')
+        'host' => '127.0.0.1',
+        'port' => '5672',
+        'user' => 'thb',
+        'password' => 'thb910626',
+        'vhost' => '/',
+        'options' => [
+            'prefix' => '',       // key 前缀
+            'max_attempts'  => 3, // 消费失败后，重试次数
+            'retry_seconds' => 5, // 重试间隔，单位秒
+        ]
     ],
 ];
