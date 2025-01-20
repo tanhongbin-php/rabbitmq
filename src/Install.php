c256525763fc40b76634371001a585f3ce2f5e79
tanhongbin-php <55487039+tanhongbin-php@users.noreply.github.com>
2025-01-20 17:07:45 +0800

    Add files via upload

--- src/Install.php
+++ src/Install.php
@@ -43,6 +43,9 @@ class Install
                     mkdir($parent_dir, 0777, true);
                 }
             }
+            if(file_exists(base_path()."/$dest")){
+                continue;
+            }
             //symlink(__DIR__ . "/$source", base_path()."/$dest");
             copy_dir(__DIR__ . "/$source", base_path()."/$dest");
             echo "Create $dest
@@ -56,6 +59,7 @@ class Install
      */
     public static function uninstallByRelation()
     {
+        return true;
         foreach (static::$pathRelation as $source => $dest) {
             $path = base_path()."/$dest";
             if (!is_dir($path) && !is_file($path)) {
